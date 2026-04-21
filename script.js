2c560e3f76457c5007489401b3962124c5dbd43f
Abubazar <sediqdude360@gmail.com>
2026-04-21 12:20:31 +0100

    added a sleek animation for the homepage of Lizen

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const canvas = document.getElementById("lookhole")
 const ctx = canvas.getContext('2d')
-
+canvas.width=200
+canvas.height=200
+ctx.imageSmoothingEnabled = false
+canvas.style.imageRendering = "pixelated"
 const char = new Image()
 char.src = "assets/Lizen/Idle.png"
 
-char.onload = playIdle()
+char.onload = function(){requestAnimationFrame(playIdle)}
 
 frame = 0
 totalFrames = 9
-function playIdle(){
+frameWidth = 162
+animSpeed=5
+step =0
 
+let lastTime = 0
+const FPS = 60
+const timestep = 1000/FPS
+
+function playIdle(ctime){
+const deltaTime = (ctime - lastTime)/1000
+    if (ctime - lastTime >= timestep){
+        ctx.clearRect(0,0,200,200)
+        ctx.drawImage(char,frameWidth + (frameWidth*frame),0,162,162, -150,-150,500,500)
+        step = (step+1)%animSpeed
+        if(step==animSpeed-1){
+            frame = (frame+1)%9
+        }
+        lastTime = ctime
+    }
+    requestAnimationFrame(playIdle)
 }
