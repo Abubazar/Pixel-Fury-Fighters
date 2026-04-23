2fd1262c5c91c05fb96abd37851148de1f008e6e
Abubazar <sediqdude360@gmail.com>
2026-04-23 13:56:13 +0100

    figuring animations for characters

--- gscript.js
+++ gscript.js
@@ -1,6 +1,8 @@
 //--CANVAS SETUP
 const canvas = document.getElementById("screen")
 const ctx = canvas.getContext('2d')
+ctx.imageSmoothingEnabled = false
+canvas.style.imageRendering = "pixelated"
 
 ctx.imageSmoothingEnabled = false
 canvas.style.imageRendering = "pixelated"
@@ -10,13 +12,79 @@ canvas.width = 1280
 
 //--GAME VARIABLES
 const FPS=60
-const gravity = 25
+const gravity = 100
 const animSpeed=5
 const ground = 650
 const players = []
 
+function UpDrawImage(img, x, y, flipX, width, height, fsW,fsH, frame) {
+    if (!img.complete) return
+
+    const sx = frame * fsW
+    const sy = 0
+    const sWidth = fsW
+    const sHeight = fsH
+
+    ctx.save()
+    if (flipX) {
+        ctx.scale(-1, 1)
+        ctx.drawImage(
+            img,
+            sx, sy, sWidth, sHeight,
+            -x - width, y, width, height
+        )
+    } else {
+        ctx.drawImage(
+            img,
+            sx, sy, sWidth, sHeight,
+            x, y, width, height
+        )
+    }
+
+    ctx.restore()
+}
+
+//--CHARACTER IMAGES
+const Axel = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Axel[name] = [new Image(),135]
+  Axel[name][0].src = `assets/Axel/${name}.png`
+})
+
+const Guantano = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Guantano[name] = [new Image(),126]
+  Guantano[name][0].src = `assets/Guantano/${name}.png`
+})
+
+const Kiara = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Kiara[name] = [new Image(),150]
+  Kiara[name][0].src = `assets/Kiara/${name}.png`
+})
+
+const Kimal = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Kimal[name] = [new Image(),100]
+  Kimal[name][0].src = `assets/Kimal/${name}.png`
+})
+
+const Lizen = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Lizen[name] = [new Image(),162]
+  Lizen[name][0].src = `assets/Lizen/${name}.png`
+})
+
+const Zaruto = {};
+["Attack1", "Attack2", "Death", "Idle", "Jump", "Run"].forEach(name => {
+  Zaruto[name] = [new Image(),100]
+  Zaruto[name][0].src = `assets/Zaruto/${name}.png`
+})
+
+
+
 class Sprite{
-    constructor(x, y, id){
+    constructor(x, y, id, character){
         this.x = x
         this.y = y
         this.id = id
@@ -25,6 +93,20 @@ class Sprite{
         this.height=200
         this.touchGrass=false
         this.isAttacking = false
+        this.animation='Idle'
+        this.flipX=false
+        this.totalFrames=0
+        this.frame=0
+        this.stepCount=0
+        this.character=character
+        this.canSwitch=true
+    }
+
+    attack(){
+        if(this.canSwitch){
+            const type = getRandomInt(1,2)
+            this.animation='Attack'+type
+        }
     }
 
     update(delta){
@@ -32,24 +114,39 @@ class Sprite{
 
         this.velocityY+=gravity
         this.y+=this.velocityY*delta
-        if(this.y > ground-this.height){
-            this.y=ground-this.height
+        if(this.y > ground){
+            this.y=ground
             this.velocityY=0
             this.touchGrass=true
         }
         else{this.touchGrass=false}
     }
     draw(){
-        ctx.fillRect(this.x,this.y,100,this.height)
+        const anim = this.animation
+        const image = this.character[anim][0]
+        const width = this.character[anim][1]
+        this.totalFrames = image.width/width
+        this.stepCount+=1
+        if(this.stepCount>=animSpeed){
+            this.frame = (this.frame+1)%this.totalFrames
+            this.stepCount=0
+        }
+        if(this.anim=='Attack1' || this.anim=='Attack2'){
+            if(this.frame==this.totalFrames){this.canSwitch=true; this.animation='Idle'}
+        }
+        UpDrawImage(image,this.x-500,this.y-620,this.flipX,1000,1000,width,image.height,this.frame)
+        
+        ctx.fillStyle='rgba(255,0,0,0.5)'
+        ctx.fillRect(this.x-50,this.y-this.height,100,this.height)
     }
 
     jump(){
-        if(this.touchGrass){this.velocityY=-900}
+        if(this.touchGrass){this.velocityY=-1800}
     }
     move(dir){
         const speed = 400
-        if(dir=='left'){this.velocityX=-speed}
-        else if(dir=='right'){this.velocityX=speed}
+        if(dir=='left'){this.velocityX=-speed; this.flipX=true}
+        else if(dir=='right'){this.velocityX=speed; this.flipX = false}
         else{this.velocityX=0}
     }
 }
@@ -57,10 +154,10 @@ class Sprite{
 const entities = []
 
 
-entities.push(new Sprite(100,20,entities.length))
+entities.push(new Sprite(100,20,entities.length,Lizen))
 players.push(0)
-entities.push(new Sprite(1100,30,entities.length))
-entities.push(new Sprite(800,30,entities.length))
+entities.push(new Sprite(1100,30,entities.length,Kiara))
+entities.push(new Sprite(800,30,entities.length,Axel))
 enemy1 = new EnemyController(1,3)
 enemy2 = new EnemyController(2,2)
 
@@ -92,7 +189,11 @@ function KeyboardUpdate(){
 
 
 function render(){
+    ctx.imageSmoothingEnabled = false
+    canvas.style.imageRendering = "pixelated"
     ctx.clearRect(0,0,canvas.width,canvas.height)
+    ctx.fillStyle = 'green'
+    ctx.fillRect(0,ground,1500,600)
     for(let i=0; i<entities.length; i++){
         entities[i].draw()
     }
